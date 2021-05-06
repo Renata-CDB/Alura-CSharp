dcdc5cf1b77c73970bb518d7e8e41ceb7d2749c6
Renata Bianchine <renatabianchine_@hotmail.com>
2021-05-06 09:55:05 -0300

    add arquivos

---
+++ 4-ConversõesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversõesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 4");
+
+
+            double salario  = 1200.50;
+
+
+
+
+            // o int é uma variavel que suporta até 32 bits
+            int SalarioEmInterio = (int)salario;
+
+
+            Console.WriteLine(SalarioEmInterio);
+
+
+            // long é uma variavel de 64 bits
+            long idade = 1300000000000;
+            Console.WriteLine(idade);
+
+
+            // O short é uma variavel que suporta até 16 bits
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 caracteres e textos");
+
+            // Character
+            char primeiraLetra = ' ';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)61;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+
+            string titulo = "alura Cursos de tecnologia " + 2020;
+            string CursosProgramacao = 
+ @" - .NET 
+ - Java
+ - JavaScript";
+                
+
+
+            Console.WriteLine(titulo);
+            Console.WriteLine(CursosProgramacao);
+
+            Console.ReadLine();
+        }
+    }
+}
